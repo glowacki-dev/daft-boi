d58291f827fd9c0f291ae0785cc3bfedb7c4d1dd
Maciej Głowacki <bombasarkadian@gmail.com>
2019-05-10 18:51:06 +0200

    Update music

--- src/StateGame.c
+++ src/StateGame.c
@@ -17,6 +17,7 @@ UINT8 bank_STATE_GAME = 2;
 extern UINT8 current_level;
 extern const UINT8 num_playable_levels;
 extern const struct LevelInfo levels[];
+extern UINT8* battle_theme_mod_Data[];
 
 UINT8 collision_tiles[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 253, 0};
 UINT8 next_level;
@@ -45,6 +46,7 @@ void Start_STATE_GAME() {
 
 	game_state.state = PLAYING;
 	game_state.door_state = 0;
+	PlayMusic(battle_theme_mod_Data, 3, 1);
 }
 
 void Update_STATE_GAME() {
